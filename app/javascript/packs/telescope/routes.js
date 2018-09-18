f561bc2325a2aeca6eb5430a1b553c66c65ae039
Nico Glennon <nicoglennon@icloud.com>
2018-09-18 14:28:20 -0500

    successfully set up the correct react app structure

---
+++ app/javascript/packs/telescope/routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Root from './components/root';
+const App = (props) => (
+  <Router>
+    <div>
+      <Route exact path='/' component={Root} />
+    </div>
+  </Router>
+)
+export default App;
