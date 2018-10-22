730775d419e5b79b1dae0c0c43abe13971eb2d84
Nico Glennon <nicoglennon@icloud.com>
2018-10-22 18:26:05 -0500

    set background for weekcards based on their percentage

--- app/javascript/packs/telescope/components/weekCard.jsx
+++ app/javascript/packs/telescope/components/weekCard.jsx
@@ -3,12 +3,16 @@ import { Link } from 'react-router-dom';
 
 class WeekCard extends React.Component {
   render(){
+    var color = "hsla(0,100%,50%, 1)";
+    var percentage = this.props.weekObj.percentage;
+    var hue=Math.round(((345 - (percentage/100)*200)).toString(10));
+    color = ["hsla(",hue,",100%,75%, 1)"].join("");
     return(
       <li className="weekCard-li" key={this.props.weekObj.id}>
-        <Link className="weekCard-alink" to={`/@${this.props.username}/weeks/${this.props.weekObj.id}`} >
+        <Link className="weekCard-alink" to={`/@${this.props.username}/weeks/${this.props.weekObj.id}`} style={{backgroundColor: color}} >
           <div className="weekCard-div">
             <p className="weekCard-date">{this.props.weekObj.date}</p>
-            <p className="weekCard-percentage">{this.props.weekObj.percentage}</p>
+            <p className="weekCard-percentage">{percentage}</p>
           </div>
         </Link>
       </li>
