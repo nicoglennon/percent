c8f15b41ce1fc213d34813901b4815996bb038f0
Nico Glennon <nicoglennon@icloud.com>
2018-10-27 18:39:45 -0500

    Modify webpacker prod config file to make z-index work

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,4 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+environment.plugins.get('OptimizeCSSAssets').options.cssProcessorOptions.safe = true
+
 module.exports = environment.toWebpackConfig()
