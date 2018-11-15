a4c7ee60f2d4548503ba8cbe6f71ad186596aa94
Nico Glennon <nicoglennon@icloud.com>
2018-11-14 20:40:10 -0600

    styling changes & new copy

--- app/javascript/packs/telescope/components/welcomePageContentFirstPage.jsx
+++ app/javascript/packs/telescope/components/welcomePageContentFirstPage.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const WelcomePageContentFirstPage = ({username}) => (
   <div>
-    <p>Hey, <strong>@{username}</strong>! <span className="welcomePageContent-emoji">👋</span></p>
+    <br />
+    <p>Hey, <strong>@{username}</strong>!<span className="welcomePageContent-emoji"> 👋</span></p>
     <p>Welcome to <strong>Datascope</strong>, a place where hundreds of people come to self-optimize every week.</p>
-    <p>Here's a quick tour:</p>
-    <p className="welcomePageContent-threefeatures">1. Your  <strong>Board</strong> <span className="welcomePageContent-emoji">🗺</span></p>
-    <p className="welcomePageContent-threefeatures">2. Your <strong>Weeks</strong> <span className="welcomePageContent-emoji">🗓</span></p>
-    <p className="welcomePageContent-threefeatures">3. Your <strong>Analytics</strong> <span className="welcomePageContent-emoji">📈</span></p>
+    <p>Here's how to get started:</p>
+    <p className="welcomePageContent-threefeatures">1. Build your  <strong>Board</strong> <span className="welcomePageContent-emoji">🗺</span></p>
+    <p className="welcomePageContent-threefeatures">2. Create your <strong>Weeks</strong> <span className="welcomePageContent-emoji">🗓</span></p>
+    <p className="welcomePageContent-threefeatures">3. Watch your <strong>Analytics</strong> <span className="welcomePageContent-emoji">📈</span></p>
     <p>Let's start with your <strong>Board</strong>.</p>
   </div>
 );
